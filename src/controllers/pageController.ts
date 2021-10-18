842508a88f048f4399683c1b5d91b08eccdcd0ae
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-18 10:57:18 -0300

    Listando os pets no controller

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 
+import {Pet} from '../models/pet'
+
 import {createMenuObject} from '../helpers/createMenuObject'
 
 
@@ -7,12 +9,15 @@ export const home = (req: Request, res: Response) =>{
 
    // res.send('home no controoler');
 
+   let list = Pet.getAll();
+
    res.render('pages/page', {
        menu: createMenuObject('all'),
        banner:{
            title: 'Todos os animais',
            background: 'allanimals.jpg'
-       }
+       },
+       list: list
    })
 
 
@@ -22,12 +27,15 @@ export const dogs = (req: Request, res: Response) =>{
 
   // res.send('dogs');
 
+  let list = Pet.getFromType('dog')
+
   res.render('pages/page', {
       menu: createMenuObject('dog'),
       banner:{
           title: 'Cachorros',
           background:'banner_dog.jpg'
-      }
+      },
+      list: list
   })
 
 
@@ -38,12 +46,15 @@ export const cats = (req: Request, res: Response) =>{
 
    // res.send('cats');
 
+   let list = Pet.getFromType('cat')
+
    res.render('pages/page', {
     menu: createMenuObject('cat'),
     banner:{
         title: 'Gatos',
         background:'banner_cat.jpg'
-    }
+    },
+    list: list
 })
 
 
@@ -54,12 +65,16 @@ export const fishes = (req: Request, res: Response) =>{
 
    // res.send('fishes');
 
+   let list = Pet.getFromType('fish')
+
    res.render('pages/page', {
     menu: createMenuObject('fish'),
     banner:{
         title: 'Peixes',
         background:'banner_fish.jpg'
-    }
+    },
+
+    list:list
 })
 
 
